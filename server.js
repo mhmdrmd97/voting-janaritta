bfcf7a2e8366296ff9997ac1597d5c081de460dc
mhmdrmd97 <68133073+mhmdrmd97@users.noreply.github.com>
2023-10-03 11:41:24 -0700

    Update server.js
    backup

--- server.js
+++ server.js
@@ -3,37 +3,14 @@ const bodyParser = require('body-parser');
 
 const app = express();
 app.use(bodyParser.json());
+let totalUsers = 2000;
 
-let userList = [
-  { id: 1, votes: 0, isVotedBefore: 'N' },
-  { id: 2, votes: 0, isVotedBefore: 'N' },
-  { id: 3, votes: 0, isVotedBefore: 'N' },
-  { id: 4, votes: 0, isVotedBefore: 'N' },
-  { id: 5, votes: 0, isVotedBefore: 'N' },
-  { id: 6, votes: 0, isVotedBefore: 'N' },
-  { id: 7, votes: 0, isVotedBefore: 'N' },
-  { id: 8, votes: 0, isVotedBefore: 'N' },
-  { id: 9, votes: 0, isVotedBefore: 'N' },
-  { id: 10, votes: 0, isVotedBefore: 'N' },
-  { id: 11, votes: 0, isVotedBefore: 'N' },
-  { id: 12, votes: 0, isVotedBefore: 'N' },
-  { id: 13, votes: 0, isVotedBefore: 'N' },
-  { id: 14, votes: 0, isVotedBefore: 'N' },
-  { id: 15, votes: 0, isVotedBefore: 'N' },
-  { id: 16, votes: 0, isVotedBefore: 'N' },
-  { id: 17, votes: 0, isVotedBefore: 'N' },
-  { id: 18, votes: 0, isVotedBefore: 'N' },
-  { id: 19, votes: 0, isVotedBefore: 'N' },
-  { id: 20, votes: 0, isVotedBefore: 'N' },
-  { id: 21, votes: 0, isVotedBefore: 'N' },
-  { id: 22, votes: 0, isVotedBefore: 'N' },
-  { id: 23, votes: 0, isVotedBefore: 'N' },
-  { id: 24, votes: 0, isVotedBefore: 'N' },
-  { id: 25, votes: 0, isVotedBefore: 'N' },
-  { id: 26, votes: 0, isVotedBefore: 'N' },
-  { id: 27, votes: 0, isVotedBefore: 'N' },
-  { id: 28, votes: 0, isVotedBefore: 'N' },
-];
+let userList = Array.from({ length: totalUsers }, (_, i) => ({ id: i + 1, votes: 0, isVotedBefore: 'N' }));
+let backup = [];
+
+backup.forEach((elem,index)=>{
+userList[elem["id"] -1] = {...userList[elem["id"] -1], ...elem}
+});
 
 app.get('/userList', (req, res) => {
   res.json(userList);
@@ -46,22 +23,23 @@ app.post('/vote/:userId/:votedId', (req, res) => {
   const user = userList.find(item => item.id === userId);
   const votedUser = userList.find(item => item.id === votedId);
 
-  if (!user || !votedUser) {
+  if (!user || !votedUser) { //if user id or voted id is not correct
     return res.status(400).json({ message: 'Invalid user or voted ID.' });
   }
 
-  if (user == votedUser) {
+  if (user == votedUser) { //if user cannot vote for himself
     return res.status(400).json({ message: 'user cannot vote for him/her self.' });
   }
 
-  if (user.isVotedBefore === 'N') {
+if(user.isVotedBefore === 'Y'){ //if user already voted he cannot vote again
+ return   res.status(400).json({ message: 'User has already voted.' });
+}
+
     votedUser.votes++;
     user.isVotedBefore = 'Y';
 
     res.json({ message: 'Vote recorded successfully.' });
-  } else {
-    res.status(400).json({ message: 'User has already voted.' });
-  }
+  
 });
 
 const PORT = process.env.PORT || 3000;
